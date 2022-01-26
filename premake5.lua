b1c90970938dc543f3caedb32dbee755e80362d5
Isaac Yong <isaacyong0804@gmail.com>
2022-01-26 23:54:37 +0800

    Premake setup

---
+++ premake5.lua
@@ -0,0 +1,100 @@
+workspace "Genesis"
+	architecture "x64"
+	configurations {"Debug", "Release", "Dist"}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Genesis"
+	location "Genesis"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs 
+	{
+		"%{prj.name}/thirdparty/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines 
+		{
+			"GS_PLATFORM_WINDOWS",
+			"GS_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{MKDIR} ../bin/" .. outputdir .. "/Sandbox"),
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "GS_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GS_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GS_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Genesis/thirdparty/spdlog/include",
+		"Genesis/src"
+	}
+
+	links
+	{
+		"Genesis"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines 
+		{
+			"GS_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "GS_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GS_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GS_DIST"
+		optimize "On"
