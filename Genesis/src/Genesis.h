e9bdbabbc564b6919fe16f64546ae549f2e49584
Isaac Yong <isaacyong0804@gmail.com>
2022-01-26 20:08:03 +0800

    Log system

--- Genesis/src/Genesis.h
+++ Genesis/src/Genesis.h
@@ -1,8 +1,10 @@
 #pragma once
 
 // For use by Genesis Application
-
 #include "Genesis/Application.h"
+
+// Logging
+#include "Genesis/Log.h"
 
 // Entry Point
 #include "Genesis/EntryPoint.h"
--- Genesis/src/Genesis/EntryPoint.h
+++ Genesis/src/Genesis/EntryPoint.h
@@ -6,6 +6,10 @@ extern Genesis::Application* Genesis::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Genesis::Log::Init();
+	GS_CORE_WARN("Initialized Core Logger!");
+	GS_INFO("Initialized Client Logger!");
+
 	auto app = Genesis::CreateApplication();
 	app->Run();
 	delete app;
